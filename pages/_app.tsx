71fb33f8b62fc70a7887afd72de4412af3d80371
Rami Bitar <rbitar@gmail.com>
2025-03-11 15:37:29 -0400

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useErrors, ErrorBoundary } from "@/hooks/use-errors";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 
+
 export default function App({ Component, pageProps }: AppProps) {
   useErrors()
   
