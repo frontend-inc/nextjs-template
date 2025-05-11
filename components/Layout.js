5b578ca78318f12714186eac6cf4a37acd4a4ecd
Rami Bitar <rbitar@gmail.com>
2025-05-11 13:19:57 -0400

    update homepage

--- components/Layout.js
+++ components/Layout.js
@@ -4,11 +4,9 @@ import Footer from './Footer';
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
       <main className="flex-1">
         {children}
       </main>
-      <Footer />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import { cn } from '../lib/utils';
 import Hero from '../components/Hero';
 
-export default function Home({ user }) {
+export default function Home() {
   return (
-    <>
-      {/* Hero Component */}
-      <Hero 
-        title="Start Prompting" 
-        subtitle="Build your next project with AI"
-      />
-    </>
+    <div className="w-full h-full flex flex-col items-center justify-start p-6">
+      <h1 className="text-4xl font-bold text-black mb-4">Start prompting</h1>
+      <p className="text-xl text-gray-700 max-w-3xl mx-auto">Build your next project with AI</p>      
+    </div>
   );
 }
