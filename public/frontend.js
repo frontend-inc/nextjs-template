cb0c34d6ebab5b15271f122c69477b330c2d12c1
Rami Bitar <rbitar@gmail.com>
2025-05-12 15:31:28 -0400

    update

--- public/frontend.js
+++ public/frontend.js
@@ -2,6 +2,5 @@
   const script = document.createElement('script');
   const timestamp = new Date().getTime();
   script.src = `https://frontend-ai.s3.us-east-2.amazonaws.com/frontend.js?t=${timestamp}`;
-  script.crossOrigin = 'anonymous';
   document.body.appendChild(script);
 })();
