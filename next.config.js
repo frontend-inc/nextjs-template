a13fe9e2f6f9eabac0a271e825a93506d5e65b53
Rami Bitar <rbitar@gmail.com>
2025-08-15 09:33:31 -0400

    Add supabase.frontend.co to remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'supabase.frontend.co',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig;
